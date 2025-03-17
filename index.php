2493cbea9b4e4ee77b4b44fdadad7c1fdc21bf32
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-03-17 17:09:35 +0000

    made major changes

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit1-05-HTML-MDL -->
+<!-- ICS2O-Unit1-08-HTML-MDL -->
 <html lang="en-ca">
   <head>
     <meta charset="utf-8" />
-    <meta name="description" content="My first MDL webpage, with favicon" />
+    <meta name="description" content="The XXX program, PHP" />
     <meta name="keywords" content="mths, icd2o" />
     <meta name="author" content="Mr. Coxall" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -34,50 +34,37 @@
       href="./favicon-16x16.png"
     />
     <link rel="manifest" href="./site.webmanifest" />
-    <title>Home Address, with JS</title>
+    <title>The XXX program, PHP</title>
   </head>
 
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <script src="./js/script.js"></script>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       <header class="mdl-layout__header">
         <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Home Address, with JS</span>
+          <span class="mdl-layout-title">The XXX program, PHP</span>
         </div>
       </header>
       <main class="mdl-layout__content">
         <div class="right-image">
-          <img src="./computeruser.png" alt="computer user" />
+          <img src="./images/notebook-animation.svg" alt="laptop image" />
         </div>
-        <div class="page-content">Please fill the space below...</div>
+        <div class="page-content">Instructions here ...</div>
         <br />
         <!-- Simple Textfield for integers-->
-        <form action="#">
-          <p>Street number</p>
+        <form action="answer.php" method="GET">
           <div class="mdl-textfield mdl-js-textfield">
             <!-- input pattern attribute -->
             <input
               class="mdl-textfield__input"
-              type="number"
+              type="text"
               pattern="-?[0-9]*(\.[0-9]+)?"
               id="length"
             />
             <!-- mdl-textfield__label -->
-            <label class="mdl-textfield__label" for="street-number"
-              >Street number</label
+            <label class="mdl-textfield__label" for="demo-input"
+              >Length of ... (mm)</label
             >
-            <!-- class "mdl-textfield__error" -->
-            <span class="mdl-textfield__error">Input is not a number</span>
-          </div>
-          <br />
-
-          <p>Street name</p>
-          <div class="mdl-textfield mdl-js-textfield">
-            <!-- input pattern attribute -->
-            <input class="mdl-textfield__input" type="number" pattern="-?[0-9]*(\.[0-9]+)?" id="length" />
-            <!-- mdl-textfield__label -->
-            <label class="mdl-textfield__label" for="street-number">Street name</label>
             <!-- class "mdl-textfield__error" -->
             <span class="mdl-textfield__error">Input is not a number</span>
           </div>
@@ -85,16 +72,12 @@
           <!-- Accent-colored raised button with ripple -->
           <button
             class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            onclick="myButtonClicked()"
-            type="button"
+            type="submit"
           >
             Calculate
           </button>
         </form>
         <br />
-        <div class="page-content-answer">
-          <div id="answer">The answer is: </div>
-        </div>
       </main>
     </div>
   </body>
